8b08f660b1224d730774c1ab74235831edcdd17e
henriSandovalSilva <42452534+henriSandovalSilva@users.noreply.github.com>
2019-05-27 22:16:08 -0300

    Adicionado FutureBuilder; Criada Interface

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ void main() async {
   runApp(
     MaterialApp(
       home: Home(),
+      theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white
+      ),
     ),
   );
 }
@@ -23,6 +27,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +39,86 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.active:
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    'Erro ao carregar dados :(',
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  ),
+                );
+              } else {
+                dolar = snapshot.data['results']['currencies']['USD']['buy'];
+                euro = snapshot.data['results']['currencies']['EUR']['buy'];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(
+                        Icons.monetization_on,
+                        size: 150.0,
+                        color: Colors.amber,
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Reais',
+                          labelStyle: TextStyle(color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: 'R\$ ',
+                        ),
+                        style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25.0,
+                        ),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Reais',
+                          labelStyle: TextStyle(color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: 'R\$ ',
+                        ),
+                        style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25.0,
+                        ),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Reais',
+                          labelStyle: TextStyle(color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: 'R\$ ',
+                        ),
+                        style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25.0,
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: new CircularProgressIndicator(),
+              );
+              break;
+          }
+        },
+      ),
     );
   }
 }
-
